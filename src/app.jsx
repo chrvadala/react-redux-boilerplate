a5038c47ee288b67c9df16c09931794929a26328
Christian Vadalà <chr.vadala@gmail.com>
2016-12-14 19:15:40 +0100

    rename components dir to ui

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import View from './components/view';
+import View from './ui/view';
 import {addNumberAction} from './actions';
 
 function mapStateToProps(reduxState) {
