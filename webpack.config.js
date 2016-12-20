5d76bdaa2147e2230e23f80ec475e8f719a91559
Christian Vadalà <chr.vadala@gmail.com>
2016-12-20 19:22:21 +0100

    migrate to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,81 @@
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var path = require('path');
-var webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/renderer.jsx'),
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
+/**
+ * --env.production
+ * --env.port port
+ * --env.title 'page title'
+ */
+
+module.exports = function (env) {
+  let isProduction = env && env.hasOwnProperty('production');
+  let port = env && env.hasOwnProperty('port') ? env.port : 8080;
+  let pageTitle = env && env.hasOwnProperty('title') ? env.title : 'Hello world!';
+
+  if (isProduction) console.info('Webpack: Production mode'); else console.info('Webpack: Development mode');
+
+  let config = {
+    context: path.resolve(__dirname),
+    performance: {
+      hints: isProduction ? 'warning' : false
+    },
+    entry: {
+      app: './src/renderer.jsx',
+      vendor: ['immutable', 'react', 'react-dom', 'react-redux', 'redux']
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[chunkhash].[name].js',
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    devServer: {
+      port: port,
+      contentBase: path.join(__dirname, './dist'),
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [{
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      }
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      }]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest'],
+        minChunks: Infinity,
+        filename: '[chunkhash].[name].js'
+      }),
+
+      new HtmlWebpackPlugin({
+        title: pageTitle,
+        template: './src/index.html.ejs',
+        filename: 'index.html',
+        inject: 'body',
+      })
     ]
-  },
-  plugins: [
-    new OpenBrowserPlugin({url: 'http://localhost:8080'})
-  ]
+  };
+
+  if (isProduction) {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+
+    config.plugins.push(new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }));
+  }
+
+  if(!isProduction){
+   config.plugins.push(new OpenBrowserPlugin({url: `http://localhost:${port}`}))
+  }
+
+  return config;
 };
